ac6bd72a7ab00ddbaf75857961f93f6bfe9aa4b9
VigneshwariRajendran <51949214+VigneshwariRajendran@users.noreply.github.com>
2019-07-09 14:36:01 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
-no=int(input())
-hi=list(map(int,input().split()[:no]))
+num=int(input())
+hii=list(map(int,input().split()[:num]))
 count=0
 fp=[]
-for i in range(0,no+1):
-   if(hi.count(i)>1):
+for i in range(0,num+1):
+   if(hii.count(i)>1):
       fp.append(i)
 if(len(fp)==0):
    print("unique")
